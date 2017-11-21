c1a50d72ec0109e73f9f8315b0b1c547e093079c
Kaur, Ishpreet <ishpreet.kaur@sogeti.com>
2017-11-21 16:57:20 +0100

    Changes in json and javascript file

--- LearningPath_DevOps/learningpath.js
+++ LearningPath_DevOps/learningpath.js
@@ -11,20 +11,15 @@ function ShowDataOnCanvas(obj) {
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
     var radius = 25;
-    var circle_Count = 3;
-    var distance_Between = canvas.width / (circle_Count + 1);
-    for (var i = 0; i < circle_Count; i++) {
-        ctx.fillText(obj.description, 50, 10);
-        ctx.beginPath();
-        ctx.arc(distance_Between * (i + 1), centerY, radius, 0, 2 * Math.PI, true);
-        //ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = '#000000';
-        ctx.stroke();
-        ctx.closePath();
-    }
+    ctx.fillText(obj.description, 50, 10);
+    ctx.beginPath();
+    //ctx.arc(distance_Between * (i + 1), centerY, radius, 0, 2 * Math.PI, true);
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#000000';
+    ctx.stroke();
     console.log(obj.description);
     // console.log(json);
 }
