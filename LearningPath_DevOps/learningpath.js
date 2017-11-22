5a2158950676a7cad31ef87222111d18986c0d53
Kaur, Ishpreet <ishpreet.kaur@sogeti.com>
2017-11-22 13:09:11 +0100

    ade changes in js file

--- LearningPath_DevOps/learningpath.js
+++ LearningPath_DevOps/learningpath.js
@@ -11,16 +11,19 @@ function ShowDataOnCanvas(obj) {
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
     var radius = 25;
-    ctx.fillText(obj.description, 50, 10);
-    ctx.beginPath();
-    //ctx.arc(distance_Between * (i + 1), centerY, radius, 0, 2 * Math.PI, true);
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
-    console.log(obj.description);
-    // console.log(json);
+    //var distance_Between = canvas.width / (circle_Count + 1);
+    var objLength = obj.length;
+    for (var i = 0; i < objLength; i++) {
+        ctx.fillText(obj.description, 50, 10);
+        ctx.beginPath();
+        // ctx.arc(distance_Between * (i + 1), centerY, radius, 0, 2 * Math.PI, true);
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
+        ctx.closePath();
+    }
+    console.log(obj.description);  
 }
-// console.log(json);
